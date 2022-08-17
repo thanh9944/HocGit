2b29b13f6f467201258c1f1851073ae9dd9328b2
Le Nhat Thanh <thanhdubai9944@gmail.com>
2022-08-17 14:45:00 +0700

    them aaa(String)

--- app/src/main/java/com/nhatthanh/hocgit/MainActivity2.java
+++ app/src/main/java/com/nhatthanh/hocgit/MainActivity2.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    String aaa="aaa";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
