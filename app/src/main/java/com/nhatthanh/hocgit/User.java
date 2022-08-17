d756bf1f84982b8a94f30699a2a9a1e1a18b0c28
Le Nhat Thanh <thanhdubai9944@gmail.com>
2022-08-17 15:17:33 +0700

    xoa id user

--- app/src/main/java/com/nhatthanh/hocgit/User.java
+++ app/src/main/java/com/nhatthanh/hocgit/User.java
@@ -1,22 +1,12 @@
 package com.nhatthanh.hocgit;
 
 public class User {
-    private int id;
     private String username;
     private String password;
 
-    public User(int id, String username, String password) {
-        this.id = id;
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getUsername() {
